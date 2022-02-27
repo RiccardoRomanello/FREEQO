09b0fd243cf3386be1aa411a19cd408dcb729715
Brian Riccardi <brian.riccardi96@gmail.com>
2022-02-27 17:28:12 +0100

    Refactor Multigraph -- Balance function

--- MultiGraph.cpp
+++ MultiGraph.cpp
@@ -4,82 +4,105 @@
 #include <assert.h>
 #include <bitset>
 #include <complex>
-#include <cmath> 
+#include <cmath>
+#include <queue>
 
-Multigraph::Multigraph(const int n) : adjacency(n, vector<vector<int> >(n, vector<int>(0))),
-degree(n, 0) {
-    nodesCount = n;
-    edgesCount = 0;
+
+void Multigraph::AddEdge(NodeLabel source, NodeLabel target) {
+    assert(source < nodesCount and target < nodesCount);
+    
+    if(Multiplicity({source, target}) == 0)
+        adjacency[source].emplace_back(target);
+
+    ++edgesCount;
+    ++in_degree[target];
+    ++out_degree[source];
+    ++multiplicity[{source, target}];
 }
 
-void Multigraph::addEdge(int source, int dest) {
-    if(source < nodesCount && dest < nodesCount){
-        adjacency[source][dest].push_back(edgesCount);
-        edgesCount++;
-        degree[source] += 1;
-        degree[dest] += 1;
-    }
-}
 
-vector<vector<complex<double> > > Multigraph::getLineGraph() {
-    vector<vector<complex<double> > > lineGraphAdjacency(edgesCount, vector<complex<double> >(edgesCount, complex<double>(0, 0)));
 
-    vector<vector<int> > rowsEquals(nodesCount, vector<int>(0)); //indexes of all rows of lineGraphAdjacency which are equals because they are all about edge inciding in j.
+std::map<Multigraph::Edge, size_t> Multigraph::Balance() {
+    std::queue<NodeLabel> B_plus, B_minus;
 
-    for(int j = 0; j < nodesCount; j++){ // fixed a j as 'middle node', I will handle all equal rows (since all edges will be incident in j)
-                                         // until I change to j+1.
-        for(int i = 0; i < nodesCount; i++){
-            vector<int> edgesFromItoJ = adjacency[i][j]; //edges ENTERING in j from i 
-
-            for(int k = 0; k < edgesFromItoJ.size(); k++){ // for each edge entering in j from i 
-                rowsEquals[j].push_back(edgesFromItoJ[k]);
-                for(int w = 0; w < nodesCount; w++){
-                    vector<int> edgesFromJtoW = adjacency[j][w]; //edges EXITING from j going to w
-                    for(int m = 0; m < edgesFromJtoW.size(); m++){
-                        lineGraphAdjacency[edgesFromItoJ[k]][edgesFromJtoW[m]] = complex<double>(1,0);
-                    }
-                }
-            }
-        }
-        sort(rowsEquals[j].begin(), rowsEquals[j].end());
+    // Retrieve surplus and deficient nodes
+    for(NodeLabel u=0; u<NodesCount(); ++u) {
+        if(Balance(u) > 0)
+            B_plus.emplace(u);
+        else if(Balance(u) < 0)
+            B_minus.emplace(u);
     }
 
-    for(int i = 0; i < nodesCount; i++){
-        vector<int> sameRowsInducedByI = rowsEquals[i];
-        int size = sameRowsInducedByI.size();
-        int hadamardColumn = 0; 
-        for(int j = 0; j < size; j++){ // for each edge inside sameRowsInducedByI
-            int currentEdge = sameRowsInducedByI[j];
-            for(int k = 0; k < edgesCount; k++){
-                if(lineGraphAdjacency[currentEdge][k] == complex<double>(1, 0)){
-                    lineGraphAdjacency[currentEdge][k] = dftCoefficient(size, j, hadamardColumn);
-                    hadamardColumn++;
-                }
+    std::map<Edge, size_t> E_bottom;
+
+    while(not B_minus.empty()) {
+        auto u = B_minus.front();
+        B_minus.pop();
+
+        auto v = B_plus.front();
+        assert(Balance(v) > 0);
+
+        while(Balance(u) < 0) {
+            assert(Balance(u) < 0 and Balance(v) > 0);
+            
+            AddEdge(u, v);
+            ++E_bottom[{u, v}];
+
+            if(Balance(v) == 0) {
+                B_plus.pop();
+                if(not B_plus.empty())
+                    v = B_plus.front();
             }
         }
     }
 
-
-    return lineGraphAdjacency;
+    return E_bottom;
 }
 
-float Multigraph::hadamardCoefficient (int size, int row, int column){
-    
-    bitset<8> rowBiset(row);
-    bitset<8> columnBiset(column); 
 
-    int result = 0;
 
-    for(int i = 0; i < 8; i++){
-        int currentDigit = ((int)rowBiset[i]) * ((int)columnBiset[i]);
-        result += currentDigit;
-    }
-    
-    return 1/pow(2,(log2(size)/2)) * pow(-1, result);
-}
 
-complex<double> Multigraph::dftCoefficient (int size, int row, int column){
-    
-    complex<double> omega(cos((2 * M_PI * row*column)/size), sin((2 * M_PI * row*column)/size));
-    return 1/pow(2,(log2(size)/2)) * omega;
-}
+
+
+
+// vector<vector<complex<double> > > Multigraph::getLineGraph() {
+//     vector<vector<complex<double> > > lineGraphAdjacency(edgesCount, vector<complex<double> >(edgesCount, complex<double>(0, 0)));
+
+//     vector<vector<int> > rowsEquals(nodesCount, vector<int>(0)); //indexes of all rows of lineGraphAdjacency which are equals because they are all about edge inciding in j.
+
+//     for(int j = 0; j < nodesCount; j++){ // fixed a j as 'middle node', I will handle all equal rows (since all edges will be incident in j)
+//                                          // until I change to j+1.
+//         for(int i = 0; i < nodesCount; i++){
+//             vector<int> edgesFromItoJ = adjacency[i][j]; //edges ENTERING in j from i 
+
+//             for(int k = 0; k < edgesFromItoJ.size(); k++){ // for each edge entering in j from i 
+//                 rowsEquals[j].push_back(edgesFromItoJ[k]);
+//                 for(int w = 0; w < nodesCount; w++){
+//                     vector<int> edgesFromJtoW = adjacency[j][w]; //edges EXITING from j going to w
+//                     for(int m = 0; m < edgesFromJtoW.size(); m++){
+//                         lineGraphAdjacency[edgesFromItoJ[k]][edgesFromJtoW[m]] = complex<double>(1,0);
+//                     }
+//                 }
+//             }
+//         }
+//         sort(rowsEquals[j].begin(), rowsEquals[j].end());
+//     }
+
+//     for(int i = 0; i < nodesCount; i++){
+//         vector<int> sameRowsInducedByI = rowsEquals[i];
+//         int size = sameRowsInducedByI.size();
+//         int hadamardColumn = 0; 
+//         for(int j = 0; j < size; j++){ // for each edge inside sameRowsInducedByI
+//             int currentEdge = sameRowsInducedByI[j];
+//             for(int k = 0; k < edgesCount; k++){
+//                 if(lineGraphAdjacency[currentEdge][k] == complex<double>(1, 0)){
+//                     lineGraphAdjacency[currentEdge][k] = dftCoefficient(size, j, hadamardColumn);
+//                     hadamardColumn++;
+//                 }
+//             }
+//         }
+//     }
+
+
+//     return lineGraphAdjacency;
+// }
--- Multigraph.hh
+++ Multigraph.hh
@@ -1,35 +1,51 @@
-#ifndef MULTIGRAPH_HH
-#define MULTIGRAPH_HH
+#pragma once
 
-#include <iostream>
+// #include <iostream>
 #include <vector>
+#include <map>
 #include <cmath>
 #include <complex>
-using namespace std;
+// using namespace std;
 
 //nodes are supposed to be just integers
 
-class Multigraph{
-    public: 
-        Multigraph(const int n);
+class Multigraph {
+    using NodeLabel = int;
+    using AdjList = std::vector<std::vector<NodeLabel>>;
+    struct Edge {
+        NodeLabel source, target;
+    };
 
-        void addEdge(int source, int destination);
-        vector<vector<complex<double> > > getLineGraph();
+
+    public:
+        Multigraph(size_t n=0) 
+        : nodesCount(n), adjacency(n), in_degree(n,0), out_degree(n,0) { }
 
 
-        int getNodesCount() const { return nodesCount; }
-        float hadamardCoefficient (int size, int row, int column);
-        complex<double> dftCoefficient (int size, int row, int column);
+        size_t Indegree(const NodeLabel u) const { return in_degree[u]; }
+        size_t Outdegree(const NodeLabel u) const { return out_degree[u]; }
+        size_t Balance(const NodeLabel u) const { return Outdegree(u) - Indegree(u); }
+        size_t Multiplicity(const Edge & uv) const { return multiplicity.contains(uv) ? multiplicity.at(uv) : 0 }
+
+        size_t NodesCount() const { return nodesCount; }
+        size_t EdgesCount() const { return edgesCount; }
+
+
+
+        std::map<Edge, size_t> Balance() {}
+
+
+
+        void AddEdge(NodeLabel, NodeLabel);
+        // vector<vector<complex<double> > > getLineGraph();
 
     private: 
+        
+        size_t nodesCount;
+        size_t edgesCount;
 
-
-        vector<vector<vector<int> > > adjacency; // a matrix nodes x nodes where every element is a list of edges (the edge has just a label)
-        vector<int> degree;  //in degree + out degree
-        int nodesCount;
-        int edgesCount;
-
-         
+        AdjList adjacency;                              // adj list
+        std::map<Edge,size_t> multiplicity;             // from (u, v) to multiplicity
+        std::vector<size_t> in_degree;                  // in_degree of a node
+        std::vector<size_t> out_degree;                 // out_degree of a node
 };
-
-#endif
